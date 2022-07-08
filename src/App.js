63bf731f8810b70ce12b7dfd1a650d966810e4e1
Barry Duffy <duffy.barry@gmail.com>
2022-07-08 15:18:51 +0100

    asdffds

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Barry
           </a>
         </header>
         <body>
