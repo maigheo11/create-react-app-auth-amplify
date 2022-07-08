827509864900eee7b25253596807fb2c76f07a35
maigheo11 <40884136+maigheo11@users.noreply.github.com>
2022-07-08 14:22:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
@@ -7,6 +8,22 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+  
+  
+  addNameToList(inFirstName,inLastName) {
+    const api = 'https://kk8q0tc5a8.execute-api.us-east-1.amazonaws.com/dev';
+    const data = { "firstName" : inFirstName, "lastName":inLastName };
+    axios
+      .post(api, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  
+  
   render() {
     return (
       <div className="App">
@@ -25,6 +42,16 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <body>
+            <form>
+                <label>First Name :</label>
+                <input type="text" id="fName">
+                <label>Last Name :</label>
+                <input type="text" id="lName">
+                <!-- set button onClick method to call function we defined passing input values as parameters -->
+                <button type="button" onclick="addNameToList(document.getElementById('fName').value,document.getElementById('lName').value)">Call API</button>
+            </form>
+        </body>
       </div>
     );
   }
